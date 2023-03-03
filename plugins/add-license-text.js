7c3f03e9809be496116afbc40993edb65ae1183f
Alex Oleksiiuk <74932289+aoleksiiuk-karsun@users.noreply.github.com>
2023-03-03 15:33:31 -0500

    Refactor build output

--- plugins/add-license-text.js
+++ plugins/add-license-text.js
@@ -9,9 +9,11 @@ class AddLicenseTextPlugin {
           delete compilation.assets[name];
         }
       }
+      const ext = process.env.NODE_ENV === 'production' ? '.min.js' : '.js';
+      const filePath = `../packed/dist/dynamic-redux-modules${ext}`;
       fs.readFile(path.join(__dirname, '../LICENSE'), 'utf8', (err, license) => {
-        fs.readFile(path.join(__dirname, '../dist/dynamic-redux-modules.js'), 'utf8', (err, bundle) => {
-            fs.writeFile(path.join(__dirname, '../dist/dynamic-redux-modules.js'), `/** \n${license}\n**/ \n ${bundle.replace('/*! For license information please see dynamic-redux-modules.js.LICENSE.txt */', '')}`, (err, data) => {});
+        fs.readFile(path.join(__dirname, filePath), 'utf8', (err, bundle) => {
+            fs.writeFile(path.join(__dirname, filePath), `/** \n${license}\n**/ \n ${bundle.replace('/*! For license information please see dynamic-redux-modules.js.LICENSE.txt */', '')}`, (err, data) => {});
         })
       });
     });
